25a59768a6e3da936dc84de8b9442e1f06676526
Hung Anh <hunganh.okokok.0@gmail.com>
2026-08-10 21:19:05 +0700

    Update 21-19-04_10-08-2026 - 06. Nhúng file tĩnh của frontend

--- project-nodejs/project-1/index.js
+++ project-nodejs/project-1/index.js
@@ -3,8 +3,12 @@ const path = require('path');
 const app = express()
 const port = 3000
 
+// Thiết lập views và view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Thiết lập thư mục chứa các tệp tĩnh (CSS, JS, hình ảnh) của Front-end
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render("client/pages/home.pug", {
