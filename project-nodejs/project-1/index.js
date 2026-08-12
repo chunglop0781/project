8f7471200b5913b99a7cd2c223c0c3c3ab678e60
Hung Anh <hunganh.okokok.0@gmail.com>
2026-08-12 11:09:17 +0700

    Update 11-09-17_12-08-2026 - Thêm biến env

--- project-nodejs/project-1/index.js
+++ project-nodejs/project-1/index.js
@@ -1,45 +1,55 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
+require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://hunganhokokok0_db_user:IV39iWz2nYOIqWDm@cluster0.wvdfh4d.mongodb.net/?appName=Cluster0', {
-}).then(() => {
-  console.log('Kết nối thành công đến MongoDB');
-}).catch((error) => {
-  console.error('Lỗi kết nối đến MongoDB:', error);
-});
-const Tour = mongoose.model('Tour', {
-  name: String,
-  vehicle: String
-});
-const app = express()
-const port = 3000
 
-// Thiết lập views và view engine
+mongoose.connect(process.env.DATABASE)
+    .then(() => {
+        console.log('Kết nối thành công đến MongoDB');
+        console.log('Database:', mongoose.connection.name);
+        console.log('Collection:', Tour.collection.name);
+    })
+    .catch((error) => {
+        console.error('Lỗi kết nối đến MongoDB:', error);
+    });
+
+const Tour = mongoose.model('Tour', {
+    name: String,
+    vehicle: String
+});
+
+const app = express();
+const port = 3000;
+
+const header = "Website Du Lịch";
+const footer = "© 2026 Website Du lịch";
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Thiết lập thư mục chứa các tệp tĩnh (CSS, JS, hình ảnh) của Front-end
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render("client/pages/home.pug", {
-    pageTitle: "Trang chủ"
-  })
-})
+    res.render("client/pages/home.pug", {
+        pageTitle: "Trang chủ",
+        header: header,
+        footer: footer
+    });
+});
 
 app.get('/tours', async (req, res) => {
-  const tourList = await Tour.find(); // Lấy danh sách tour từ cơ sở dữ liệu
+    const tourList = await Tour.find();
 
-  console.log(tourList); // In ra danh sách tour để kiểm tra
+    console.log(tourList);
 
-  res.render("client/pages/tour-list.pug", {
-    pageTitle: "Danh sách tour",
-    tourList: tourList
-  })
-})
+    res.render("client/pages/tour-list.pug", {
+        pageTitle: "Danh sách tour",
+        tourList: tourList,
+        header: header,
+        footer: footer
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Website đang chạy trên cổng ${port}`)
-})
-
-
+    console.log(`Website đang chạy trên cổng ${port}`);
+});
