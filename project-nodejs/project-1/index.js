d4a4c8ba1d9a3209b953a9bf007ba1a59f0862af
Hung Anh <hunganh.okokok.0@gmail.com>
2026-08-12 10:33:26 +0700

    Update 10-33-26_12-08-2026 - 10. Kết nối Project 1 với cơ sở dữ liệu

--- project-nodejs/project-1/index.js
+++ project-nodejs/project-1/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const path = require('path');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://hunganhokokok0_db_user:IV39iWz2nYOIqWDm@cluster0.wvdfh4d.mongodb.net/?appName=Cluster0', {
+}).then(() => {
+  console.log('Kết nối thành công đến MongoDB');
+}).catch((error) => {
+  console.error('Lỗi kết nối đến MongoDB:', error);
+});
+const Tour = mongoose.model('Tour', {
+  name: String,
+  vehicle: String
+});
 const app = express()
 const port = 3000
 
@@ -16,13 +27,19 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/tours', (req, res) => {
+app.get('/tours', async (req, res) => {
+  const tourList = await Tour.find(); // Lấy danh sách tour từ cơ sở dữ liệu
+
+  console.log(tourList); // In ra danh sách tour để kiểm tra
+
   res.render("client/pages/tour-list.pug", {
-    pageTitle: "Danh sách tour"
+    pageTitle: "Danh sách tour",
+    tourList: tourList
   })
 })
 
 app.listen(port, () => {
   console.log(`Website đang chạy trên cổng ${port}`)
 })
+
 
