6a97663deb2388c609f68443290fd4a7f6cd680e
Hung Anh <hunganh.okokok.0@gmail.com>
2026-08-15 21:20:00 +0700

    Update 21-20-00_15-08-2026 - 2.1. Tạo trang Chi tiết tour

--- project-nodejs/project-1/controllers/client/tour.controller.js
+++ project-nodejs/project-1/controllers/client/tour.controller.js
@@ -19,6 +19,27 @@ module.exports.list = async (req, res) => {
     }
 };
 
+
+
+module.exports.detail = async (req, res) => {
+    try {
+        const tourList = await Tour.find();
+
+        console.log("Danh sách tour:", tourList);
+
+        res.render("client/pages/tour-detail", {
+            pageTitle: "Chi tiết tour",
+            tourList: tourList
+        });
+    } catch (error) {
+        console.error("===== LOI LAY TOUR =====");
+        console.error(error);
+        console.error("========================");
+
+        res.status(500).send(error.message);
+    }
+};
+
 // module.exports.create = async (req, res) => {
 //     try {
 //         const tourList = await Tour.find();
--- project-nodejs/project-1/routes/client/tour.route.js
+++ project-nodejs/project-1/routes/client/tour.route.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const tourController = require('../../controllers/client/tour.controller');
 
 router.get('/', tourController.list);
+
+router.get('/detail', tourController.detail);
+
 // router.get('/create', tourController.create)
 // router.get('/delete', tourController.delete)
 // router.get('/edit', tourController.edit)
